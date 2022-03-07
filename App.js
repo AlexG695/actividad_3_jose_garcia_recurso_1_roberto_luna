a24f494de0b041b9ae8df0a3de13c1cb0b79e405
AlexG695 <alexg695845@gmail.com>
2022-03-07 16:27:08 -0700

    Creación del proyecto e integración de la api

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
+import React, {useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , FlatList, ActivityIndicator} from 'react-native';
 
 export default function App() {
+
+
+  const [datos, setData] = useState([]);
+  const[isLoading, setLoading] = useState(true);
+
+  
+
+
+  const getPosts = async() => {
+
+    try {
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      //consumir los datos
+
+      const response =  await fetch(url);
+
+      //convertir a json
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally{
+      setLoading(false);
+    }
+    
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        isLoading ? <ActivityIndicator /> : (
+          <FlatList 
+            data = {datos}
+            keyExtractor = {({id}, index) => id}
+            renderItem = {
+            ({item}) => (
+              <Text>
+                {item.title}
+              </Text>
+            )
+          }
+        />
+          
+      )}
+      
     </View>
   );
 }
